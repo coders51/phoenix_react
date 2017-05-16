e3fb37b1cb222cba794f3b78338ec4d7181274f2
Davide Dall'Olio <davide.dallolio.dev@gmail.com>
2017-05-16 17:24:28 +0200

    ddl: add deliver

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 set -e
 
-mix edeliver build release --skip-git-clean --skip-mix-clean
-mix edeliver deploy release to production
-mix edeliver restart production
-mix edeliver migrate production
-mix edeliver restart production
-mix edeliver ping production
+if [ "$#" -ne 1 ]
+then
+  echo "Usage: deploy <production|staging>"
+  exit 1
+fi
+
+_environment="$1"
+
+if [ "$_environment" = "production" ]; then
+  mix edeliver build release
+  mix edeliver deploy release to production
+  mix edeliver restart production
+  mix edeliver migrate production
+  mix edeliver restart production
+  mix edeliver ping production
+fi
+
+if [ "$_environment" = "staging" ]; then
+  mix edeliver build release --branch=staging --mix-env=staging
+  mix edeliver deploy release to staging --mix-env=staging
+  mix edeliver restart staging --mix-env=staging
+  mix edeliver migrate staging --mix-env=staging
+  mix edeliver restart staging --mix-env=staging
+  mix edeliver ping staging --mix-env=staging
+fi
